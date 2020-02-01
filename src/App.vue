86a4f1ea41bd3ee91f851d550603e6d42a489c9a
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-02-01 13:51:51 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,145 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <md-card>
+      <md-card-header>
+        <div class="md-card-title">ToDo List</div>
+      </md-card-header>
+      <md-card-content class="md-card-content">
+        <md-field class="md-field">
+          <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo.."></md-input>
+        </md-field>
+        <div class="todo-list">
+          <ul class="todos">
+            <li class="todo-item" v-for="todo in todos" :key="todo.id">
+              <span>
+                <md-checkbox
+                  class="completed md-primary"
+                  type="checkbox"
+                  @change="toggleCompleted"
+                  v-model="todo.completed"
+                />
+                <span
+                  v-if="todo.editing === false"
+                  class="todo-item-label"
+                  :class="{'is-complete' : todo.completed}"
+                  @dblclick="editTodo(todo)"
+                >{{ todo.label }}</span>
+                <div v-if="todo.editing">
+                  <md-field>
+                    <md-input
+                      type="text"
+                      v-model="todo.label"
+                      @keyup.enter="stopEdit(todo)"
+                      @keyup.esc="stopEdit(todo)"
+                      @focusout="stopEdit(todo)"
+                      placeholder="Edit.."
+                    />
+                  </md-field>
+                </div>
+              </span>
+              <span>
+                <md-button class="md-fab md-mini md-primary" @click="editTodo(todo)">
+                  <md-icon>edit</md-icon>
+                  <md-tooltip md-direction="top" md-delay="1000">edit</md-tooltip>
+                </md-button>
+                <md-button class="md-fab md-mini" @click="removeTodo(todo)">
+                  <md-icon>delete</md-icon>
+                  <md-tooltip md-direction="top" md-delay="1000">delete</md-tooltip>
+                </md-button>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </md-card-content>
+    </md-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false
+      });
+      this.currentTodo = ""; //reset input field
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.editing = true;
+    },
+    stopEdit(todo) {
+      todo.editing = false;
+    },
+    toggleCompleted() {
+      this.todos.completed = !this.todos.completed;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.md-card {
+  max-width: 80%;
+  margin: auto;
+}
+
+.md-card-header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  background-color: #00004c;
+}
+
+.md-card-title {
+  font-size: 32px;
+}
+
+.md-card-content {
+  margin-left: 1%;
+  font-size: 22px;
+}
+
+.md-field {
+  min-width: 40%;
+  max-width: 50%;
+  font-size: 22px;
+  border-bottom: 1px dotted #808080;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.todo-item {
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 0.5px solid #808080;
+}
+
+.todo-item-label {
+  color: #fff;
+  margin-left: 1%;
+}
+
+.is-complete {
+  text-decoration: line-through;
+  color: #808080;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  MdCard,
+  MdIcon,
+  MdField,
+  MdButton,
+  MdTooltip,
+  MdCheckbox
+} from 'vue-material/dist/components';
+
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
+
+Vue.use(MdCard);
+Vue.use(MdIcon);
+Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdTooltip);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
 
